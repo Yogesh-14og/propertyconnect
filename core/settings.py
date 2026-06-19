2a3c51823ab100da773e3ab271050d57a4f39fef
Krishna Bahadur Ojha <ojhakrishnabahadur010@gmail.com>
2026-06-19 13:48:46 +0545

    improved

--- core/settings.py
+++ core/settings.py
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Development मा console मा email देखाउने
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.example.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True

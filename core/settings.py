bf7bde6384fe8651d01a06158e83e7986ffade31
Krishna Bahadur Ojha <ojhakrishnabahadur010@gmail.com>
2026-06-19 11:55:37 +0545

    Production settings ready

--- core/settings.py
+++ core/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cg*-r-tlgpf8f7(xu(zlv515qt83(()^l5zh9)rtu$l4zkzva#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['dejavu14.pythonanywhere.com']
 
 # Application definition
 
@@ -117,8 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
